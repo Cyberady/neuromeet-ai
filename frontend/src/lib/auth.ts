6427c2c15d636c228d21abe8dd33f1d3a94a31cc
Cyberady <adityadbce@gmail.com>
2026-04-18 01:03:42 +0530

    fix cookies for oauth

--- frontend/src/lib/auth.ts
+++ frontend/src/lib/auth.ts
@@ -1,5 +1,8 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: `${import.meta.env.VITE_AUTH_URL}/api/auth`// Base URL of the auth server
+  baseURL: `${import.meta.env.VITE_AUTH_URL}/api/auth`,
+  fetchOptions: {
+    credentials: "include", // 🔥 REQUIRED FOR COOKIES
+  },
 });
